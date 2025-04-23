234e63ad404e88b6969852b790c39a88d872d415
Mahesh3000 <mahesh.sivngi@gmail.com>
2025-04-23 16:28:57 -0500

    deployment chsnges-3

--- src/containers/constants.js
+++ src/containers/constants.js
@@ -1,17 +1,18 @@
 const API_URLS = {
-  SIGNUP_API_URL: "http://44.206.196.76:5000/auth/register",
-  LOGIN_API_URL: "http://44.206.196.76:5000/auth/login",
-  SEND_OTP_URL: "http://44.206.196.76:5000/otp/send-email-otp",
-  GENERATE_QR_URL: "http://44.206.196.76:5000/otp/generate-qr",
-  VERIFY_TOTP_URL: "http://44.206.196.76:5000/otp/verify-totp",
-  VERIFY_EMAIL_URL: "http://44.206.196.76:5000/otp/verify-email",
-  VERIFIED_EMAILS_LIST: "http://44.206.196.76:5000/otp/list-verified-emails",
-  GET_USER: "http://44.206.196.76:5000/auth/get-user",
-  REQUEST_OTP_URL: "http://44.206.196.76:5000/otp/request-otp",
-  VERIFY_OTP_URL: "http://44.206.196.76:5000/otp/verify-otp",
-  TOGGLE_TOTP: "http://44.206.196.76:5000/auth/toggle-totp",
-  TOGGLE_OTP: "http://44.206.196.76:5000/auth/toggle-otp",
-  GOOGLE_LOGIN: "http://44.206.196.76:5000/auth/google",
+  SIGNUP_API_URL: "https://loginkit.maheshsivangi.tech/auth/register",
+  LOGIN_API_URL: "https://loginkit.maheshsivangi.tech/auth/login",
+  SEND_OTP_URL: "https://loginkit.maheshsivangi.tech/otp/send-email-otp",
+  GENERATE_QR_URL: "https://loginkit.maheshsivangi.tech/otp/generate-qr",
+  VERIFY_TOTP_URL: "https://loginkit.maheshsivangi.tech/otp/verify-totp",
+  VERIFY_EMAIL_URL: "https://loginkit.maheshsivangi.tech/otp/verify-email",
+  VERIFIED_EMAILS_LIST:
+    "https://loginkit.maheshsivangi.tech/otp/list-verified-emails",
+  GET_USER: "https://loginkit.maheshsivangi.tech/auth/get-user",
+  REQUEST_OTP_URL: "https://loginkit.maheshsivangi.tech/otp/request-otp",
+  VERIFY_OTP_URL: "https://loginkit.maheshsivangi.tech/otp/verify-otp",
+  TOGGLE_TOTP: "https://loginkit.maheshsivangi.tech/auth/toggle-totp",
+  TOGGLE_OTP: "https://loginkit.maheshsivangi.tech/auth/toggle-otp",
+  GOOGLE_LOGIN: "https://loginkit.maheshsivangi.tech/auth/google",
 };
 
 export { API_URLS };
