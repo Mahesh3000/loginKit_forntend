c0cd4b0a3af68d6b9b1151628086ae645c91bc98
Mahesh3000 <mahesh.sivngi@gmail.com>
2025-04-24 15:31:52 -0500

    cors changes

--- src/containers/constants.js
+++ src/containers/constants.js
@@ -1,18 +1,18 @@
 const API_URLS = {
-  SIGNUP_API_URL: "https://loginkit.maheshsivangi.tech/auth/register",
-  LOGIN_API_URL: "https://loginkit.maheshsivangi.tech/auth/login",
-  SEND_OTP_URL: "https://loginkit.maheshsivangi.tech/otp/send-email-otp",
-  GENERATE_QR_URL: "https://loginkit.maheshsivangi.tech/otp/generate-qr",
-  VERIFY_TOTP_URL: "https://loginkit.maheshsivangi.tech/otp/verify-totp",
-  VERIFY_EMAIL_URL: "https://loginkit.maheshsivangi.tech/otp/verify-email",
+  SIGNUP_API_URL: "https://api.loginkit.maheshsivangi.tech/auth/register",
+  LOGIN_API_URL: "https://api.loginkit.maheshsivangi.tech/auth/login",
+  SEND_OTP_URL: "https://api.loginkit.maheshsivangi.tech/otp/send-email-otp",
+  GENERATE_QR_URL: "https://api.loginkit.maheshsivangi.tech/otp/generate-qr",
+  VERIFY_TOTP_URL: "https://api.loginkit.maheshsivangi.tech/otp/verify-totp",
+  VERIFY_EMAIL_URL: "https://api.loginkit.maheshsivangi.tech/otp/verify-email",
   VERIFIED_EMAILS_LIST:
-    "https://loginkit.maheshsivangi.tech/otp/list-verified-emails",
-  GET_USER: "https://loginkit.maheshsivangi.tech/auth/get-user",
-  REQUEST_OTP_URL: "https://loginkit.maheshsivangi.tech/otp/request-otp",
-  VERIFY_OTP_URL: "https://loginkit.maheshsivangi.tech/otp/verify-otp",
-  TOGGLE_TOTP: "https://loginkit.maheshsivangi.tech/auth/toggle-totp",
-  TOGGLE_OTP: "https://loginkit.maheshsivangi.tech/auth/toggle-otp",
-  GOOGLE_LOGIN: "https://loginkit.maheshsivangi.tech/auth/google",
+    "https://api.loginkit.maheshsivangi.tech/otp/list-verified-emails",
+  GET_USER: "https://api.loginkit.maheshsivangi.tech/auth/get-user",
+  REQUEST_OTP_URL: "https://api.loginkit.maheshsivangi.tech/otp/request-otp",
+  VERIFY_OTP_URL: "https://api.loginkit.maheshsivangi.tech/otp/verify-otp",
+  TOGGLE_TOTP: "https://api.loginkit.maheshsivangi.tech/auth/toggle-totp",
+  TOGGLE_OTP: "https://api.loginkit.maheshsivangi.tech/auth/toggle-otp",
+  GOOGLE_LOGIN: "https://api.loginkit.maheshsivangi.tech/auth/google",
 };
 
 export { API_URLS };
